4881b418024337a1febfd0b4a6451fde205468d7
Laica-Lunasys <laica_lunasys@live.jp>
2013-09-23 18:16:15 +0900

    Fixed Exception

---
+++ src/me/mjolnir/mineconomy/exceptions/AccountNameConflictException.java
@@ -0,0 +1,46 @@
+package me.mjolnir.mineconomy.exceptions;
+
+/**
+ * Thrown when two accounts have the same name.
+ * 
+ * @author MjolnirCommando
+ */
+@SuppressWarnings("serial")
+public class AccountNameConflictException extends RuntimeException implements MCException
+{
+	private String method;
+	private String variable;
+
+	/**
+	 * Creates new AccountNameConflictException object.
+	 * 
+	 * @param method
+	 * @param variable
+	 */
+	public AccountNameConflictException(String method, String variable)
+	{
+		super();
+		this.method = method;
+		this.variable = variable;
+	}
+
+	public String getMethodCause()
+	{
+		return method;
+	}
+
+	public String getVariableCause()
+	{
+		return variable;
+	}
+
+	public String getErrorDescription()
+	{
+		return "The account name requested already belonged to an existing account.";
+	}
+
+	public AccountNameConflictException getError()
+	{
+		return this;
+	}
+}
